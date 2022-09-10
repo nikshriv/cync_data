1fffbdb11ef56603423194c1ffc513d40fb27651
nikshriv <nikshriv@gmail.com>
2022-09-10 12:16:06 -0700

    Update cync_data.py

--- cync_data.py
+++ cync_data.py
@@ -56,7 +56,7 @@ async def main():
     rooms = []
     for device in devices:
         device_info = await get_properties(access_token, device['product_id'], device['id'])
-        rooms.append('device':device, 'device_info':device_info)
+        rooms.append({'device':device, 'device_info':device_info})
     with open("cbyge_rooms.json","w") as file:
         file.write(json.dumps({'data': rooms},indent=4))
     print("finished getting devices, check cbyge_rooms.json for results")
